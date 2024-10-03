61db58d853f9374b147fd394edf70a8ef286e543
davidsarte <davidericsarte@gmail.com>
2024-10-03 09:52:57 -0700

    Update 3. Data Transforming.sql
    Added hourly column

--- 3. Data Transforming.sql
+++ 3. Data Transforming.sql
@@ -1,6 +1,6 @@
 ALTER TABLE  cyclistic_combined cyclistic_combined ADD COLUMN ride_length INTERVAL; 
-
-UPDATE cyclistic_combined SET ride_length = ended_at - started_at;
+UPDATE cyclistic_combined 
+SET ride_length = ended_at - started_at;
 
 ALTER TABLE cyclistic_combined ADD COLUMN month VARCHAR(2);
 
@@ -12,9 +12,12 @@ ADD COLUMN day_of_week VARCHAR(9);
 
 UPDATE cyclistic_combined
 SET day_of_week = TRIM(TO_CHAR(started_at, 'Day'));
-SELECT DISTINCT day_of_week
-FROM cyclistic_combined
-ORDER BY day_of_week;
+
+SELECT DISTINCT day_of_week;
+
+ALTER TABLE cyclistic_combined ADD COLUMN hour_of_day INTEGER;
+UPDATE cyclistic_combined
+SET hour_of_day = EXTRACT(HOUR FROM started_at);
 
 
 --Export
